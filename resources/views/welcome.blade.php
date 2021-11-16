8c7a38f446b87202da49ac63a14f3e749924b179
Antonio Martínez González <antonio_martinez_tsapp1oa2021@cev.com>
2021-11-16 16:11:37 +0100

    gitKrakenOrigin

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GitKraken</title>
+</head>
+<body>
+    <h2>Esto es GitKraKen</h2>
+    <a href="{{ route('nombre') }}"><button>Nombre</button></a>
+    <a href="{{ route('nacimiento') }}"><button>Fecha de Naciemiento</button></a>
+    <a href="{{ route('dni') }}"><button>DNI</button></a>
+</body>
+</html>
